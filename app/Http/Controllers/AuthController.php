7440f5eeb300447feaa524096e7fb5ef2a4ce20e
Zardasht Rwandzi <35077108+codezardasht@users.noreply.github.com>
2023-02-03 20:10:21 +0300

    ADD USER REGISTER IN USER MODEL

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserStoreRequest;
+use App\Models\Customer;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 use Spatie\Permission\Traits\HasRoles;
 class AuthController extends Controller
 {
@@ -32,6 +39,27 @@ class AuthController extends Controller
         }
 
         return $this->respondWithToken($token);
+    }
+
+    public function register(UserStoreRequest $request)
+    {
+
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->created_by =\auth()->id();
+        $user->created_at =Carbon::now();
+
+        return $user->save()
+            ? response()->json([
+                'status' => true,
+                "message" => "User Created Successfully",
+            ])
+            : response()->json([
+                'status' => false,
+                "message" => "Please Try Again !",
+            ], 403);
     }
 
     /**
