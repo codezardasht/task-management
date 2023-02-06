4b238208328e26c0a1ea60ec9f8cb9557a6d0711
Zardasht Rwandzi <35077108+codezardasht@users.noreply.github.com>
2023-02-07 01:11:27 +0300

    ADD SEEDER FOR THIS QUERY

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 class DatabaseSeeder extends Seeder
 {
@@ -18,94 +19,93 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        $getRole = Role::where('name','Super Admin')->first();
-        if(!$getRole)
-        {
-            $getRole =  Role::create([
-                'name' => 'Super Admin',
-                'guard_name'=>'api'
-            ]);
-        }
+        $permissions = [['name' => 'create_user', 'guard_name' => 'api'],
+            ['name' => 'update_user', 'guard_name' => 'api'],
+            ['name' => 'delete_user', 'guard_name' => 'api'],
+            ['name' => 'view_user', 'guard_name' => 'api'],
+            ['name' => 'view_board', 'guard_name' => 'api'],
+            ['name' => 'create_board', 'guard_name' => 'api'],
+            ['name' => 'update_board', 'guard_name' => 'api'],
+            ['name' => 'delete_board', 'guard_name' => 'api'],
+            ['name' => 'view_task', 'guard_name' => 'api'],
+            ['name' => 'create_task', 'guard_name' => 'api'],
+            ['name' => 'delete_task', 'guard_name' => 'api'],
+            ['name' => 'update_task', 'guard_name' => 'api'],
+            ['name' => 'delete_label', 'guard_name' => 'api'],
+            ['name' => 'update_label', 'guard_name' => 'api'],
+            ['name' => 'create_label', 'guard_name' => 'api'],
+            ['name' => 'view_label', 'guard_name' => 'api'],
+        ];
 
-        $getAdmin = User::where('email','zardasht@gmail.com')->first();
-        if(!$getAdmin)
-        {
-            $insertAdmin = User::create([
-                'name' => fake()->name(),
-                'email' => 'admin@demo.com',
-                'role_id' => $getRole->id,
-                'created_by' => 1,
-                'password' => Hash::make('12345678'), // password
-            ]);
-            $insertAdmin->assignRole($getRole->id);
+        foreach ($permissions as $permission) {
+            if (!DB::table('permissions')->where('name', $permission[ 'name' ])->exists()) {
+                DB::table('permissions')->insert($permission);
+            }
         }
+        // Create the Super Admin role
+        $superAdminRole = Role::firstOrCreate(['name' => 'Super Admin']);
 
-        DB::table('permissions')->insert([
-            [
-                'name' => 'create_user',
-                'guard_name' => 'api'
-            ],
-            [
-                'name' => 'update_user',
-                'guard_name' => 'api'
-            ],
-            [
-                'name' => 'delete_user',
-                'guard_name' => 'api'
-            ],
-            [
-                'name' => 'view_user',
-                'guard_name' => 'api'
-            ],
-            [
-                'name' => 'view_board',
-                'guard_name' => 'api'
-            ],
-            [
-                'name' => 'create_board',
-                'guard_name' => 'api'
-            ],
-            [
-                'name' => 'update_board',
-                'guard_name' => 'api'
-            ],
-            [
-                'name' => 'delete_board',
-                'guard_name' => 'api'
-            ],
-            [
-                'name' => 'view_task',
-                'guard_name' => 'api'
-            ],
-            [
-                'name' => 'create_task',
-                'guard_name' => 'api'
-            ],
-            [
-                'name' => 'delete_task',
-                'guard_name' => 'api'
-            ],
-            [
-                'name' => 'update_task',
-                'guard_name' => 'api'
-            ],
-            [
-                'name' => 'delete_label',
-                'guard_name' => 'api'
-            ],
-            [
-                'name' => 'update_label',
-                'guard_name' => 'api'
-            ],
-            [
-                'name' => 'create_label',
-                'guard_name' => 'api'
-            ],
-            [
-                'name' => 'view_label',
-                'guard_name' => 'api'
-            ],
+// Create the Product Owner role
+        $productOwnerRole = Role::firstOrCreate(['name' => 'Product Owner']);
+
+// Create the Developer role
+        $developerRole = Role::firstOrCreate(['name' => 'developers']);
+
+// Create the Tester role
+        $testerRole = Role::firstOrCreate(['name' => 'testers']);
+
+// Get all the permissions
+        $permissions = Permission::all();
+
+// Create the Super Admin user and assign all the permissions to the role
+        $superAdminUser = User::firstOrCreate([
+            'email' => 'admin@gateway.com',
+        ], [
+            'name' => 'Super Admin',
+            'role_id' => $superAdminRole->id,
+            'created_by' => 1,
+            'password' => Hash::make('secret'),
         ]);
+        $superAdminUser->assignRole($superAdminRole);
+        $superAdminRole->syncPermissions($permissions);
+
+// Create the Product Owner user and assign all the permissions to the role
+        $productOwnerUser = User::firstOrCreate([
+            'email' => 'product_owner@gateway.com',
+        ], [
+            'name' => 'Product Owner',
+            'role_id' => $productOwnerRole->id,
+            'created_by' => 1,
+            'password' => Hash::make('secret'),
+        ]);
+        $productOwnerUser->assignRole($productOwnerRole);
+        $productOwnerRole->syncPermissions($permissions);
+
+// Create the Developer user and assign only the "view_board" and "view_task" permissions to the role
+        $developerUser = User::firstOrCreate([
+            'email' => 'developer@gateway.com',
+        ], [
+            'name' => 'Developer',
+            'role_id' => $developerRole->id,
+            'created_by' => 1,
+            'password' => Hash::make('secret'),
+        ]);
+        $developerUser->assignRole($developerRole);
+        $developerRole->syncPermissions(Permission::whereIn('name', ['view_board', 'view_task'])->get());
+
+// Create the Tester user and assign only the "view_board" and "view_task" permissions to the role
+        $testerUser = User::firstOrCreate([
+            'email' => 'tester@gateway.com',
+        ], [
+            'name' => 'Tester',
+            'role_id' => $testerRole->id,
+            'created_by' => 1,
+            'password' => Hash::make('secret'),
+        ]);
+        $testerUser->assignRole($testerRole);
+        $testerRole->syncPermissions(Permission::whereIn('name', ['view_board', 'view_task'])->get());
+
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
