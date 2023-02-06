cd0353c300c178cdee0b9e3a5d2cd4b0f6b66aed
Zardasht Rwandzi <35077108+codezardasht@users.noreply.github.com>
2023-02-06 09:33:02 +0300

    ADD ROLE SEEDER

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+
+        DB::table('permissions')->insert([
+            [
+                'name' => 'create_user',
+                'guard_name' => 'web'
+            ],
+            [
+                'name' => 'update_user',
+                'guard_name' => 'web'
+            ],
+            [
+                'name' => 'delete_user',
+                'guard_name' => 'web'
+            ],
+            [
+                'name' => 'view_user',
+                'guard_name' => 'web'
+            ],
+            [
+                'name' => 'view_board',
+                'guard_name' => 'web'
+            ],
+            [
+                'name' => 'create_board',
+                'guard_name' => 'web'
+            ],
+            [
+                'name' => 'update_board',
+                'guard_name' => 'web'
+            ],
+            [
+                'name' => 'delete_board',
+                'guard_name' => 'web'
+            ],
+            [
+                'name' => 'view_task',
+                'guard_name' => 'web'
+            ],
+            [
+                'name' => 'create_task',
+                'guard_name' => 'web'
+            ],
+            [
+                'name' => 'delete_task',
+                'guard_name' => 'web'
+            ],
+            [
+                'name' => 'update_task',
+                'guard_name' => 'web'
+            ],
+            [
+                'name' => 'delete_label',
+                'guard_name' => 'web'
+            ],
+            [
+                'name' => 'update_label',
+                'guard_name' => 'web'
+            ],
+            [
+                'name' => 'create_label',
+                'guard_name' => 'web'
+            ],
+            [
+                'name' => 'view_label',
+                'guard_name' => 'web'
+            ],
+        ]);
+
+        $productOwnerRole = Role::create(['name' => 'Product Owner']);
+        $developerRole = Role::create(['name' => 'Developer']);
+        $testerRole = Role::create(['name' => 'Tester']);
+
+        $productOwnerRole->givePermissionTo([
+            'create_user',
+            'update_user',
+            'delete_user',
+            'view_user',
+            'create_board',
+            'update_board',
+            'delete_board',
+            'view_board',
+            'create_task',
+            'update_task',
+            'delete_task',
+            'view_task',
+            'create_label',
+            'update_label',
+            'delete_label',
+            'view_label',
+
+        ]);
+
+    }
+}
